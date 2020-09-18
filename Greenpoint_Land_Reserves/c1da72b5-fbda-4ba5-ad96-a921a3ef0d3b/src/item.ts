ed3267f5cd3c97b8e51739b1c56c26e8a36a1c08
gumshed <gumshed@protonmail.com>
2020-09-18 13:49:19 -0500

    Current Greenpoint config deployed 9.18.20

---
+++ Greenpoint_Land_Reserves/c1da72b5-fbda-4ba5-ad96-a921a3ef0d3b/src/item.ts
@@ -0,0 +1,47 @@
+export type Props = {
+  text?: string
+  fontSize?: number
+}
+
+type ChangeTextType = { newText: string }
+
+export default class SignPost implements IScript<Props> {
+  init() { }
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const sign = new Entity()
+    sign.setParent(host)
+
+    sign.addComponent(new GLTFShape('c1da72b5-fbda-4ba5-ad96-a921a3ef0d3b/models/signpost/SignPost_Square.glb'))
+
+    let signText = new Entity()
+    signText.setParent(host)
+    let text = new TextShape(props.text)
+    text.fontSize = props.fontSize
+    text.color = Color3.Black()
+
+    text.outlineColor = Color3.Black()
+    text.outlineWidth = 0.3
+
+    text.width = 20
+    text.height = 10
+    text.hTextAlign = 'center'
+
+    signText.addComponent(text)
+
+    signText.addComponent(
+      new Transform({
+        position: new Vector3(0, 2.05, -0.03),
+        rotation: Quaternion.Euler(0, 0, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+
+    channel.handleAction<ChangeTextType>('changeText', action => {
+      text.value = action.values.newText
+    })
+
+    channel.request<string>('getText', signText => (text.value = signText))
+    channel.reply<string>('getText', () => text.value)
+  }
+}
